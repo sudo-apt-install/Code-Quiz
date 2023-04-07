a213a6534ad5e0d90e1d559cbc2648b19e10cb28
Marq W <marquisewest86@duck.com>
2023-04-06 19:39:18 -0500

    completely restuctured how the code was written

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+var body = document.body;
+var h1El = document.createElement("h1");
+var p1El = document.createElement("p");
+var card = document.createElement("div");
+var cardHead = document.createElement("div");
+var cardBody = document.createElement("div");
+var cardFooter = document.createElement("div");
+var infoEl = document.createElement("div");
+
+//Creating a list element
+var listElement = document.createElement("ol");
+
+//Creating ordered list items
+var li1 = document.createElement("li");
+var li2 = document.createElement("li");
+var li3 = document.createElement("li");
+var li4 = document.createElement("li");
+
+h1El.textContent = "Dragon Ball (qui)Z";
+p1El.textContent = "Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds!"
